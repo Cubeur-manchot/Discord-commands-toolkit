1680125450be2e0bf19fd5b8fd54a363bec200cf
Cubeur-manchot <matthieu.aubert17@gmail.com>
2026-06-21 01:56:23 +0200

    Implement entrypoint for NPM package

---
+++ index.js
@@ -0,0 +1,11 @@
+export {default as Command} from "./command.js";
+export {default as CommandContexts} from "./commandContexts.js";
+export {default as CommandDeployer} from "./commandDeployer.js";
+
+export {
+	SlashCommandOption,
+	SlashCommandStringOption,
+	SlashCommandUserOption,
+	SlashCommandIntegerOption,
+	SlashCommandBooleanOption
+} from "./slashCommandOption.js";
